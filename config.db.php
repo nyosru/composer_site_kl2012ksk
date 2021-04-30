491f1f69ebc8e3f431e758cc511ddfb0f1a338d5
Sergey <1@php-cat.com>
2021-05-01 04:31:32 +0500

    1.4.0

--- config.db.php
+++ config.db.php
@@ -1,6 +1,21 @@
 <?php
 
+
+
+
+// для входа
+\Didrive\AUT::$vk_app_id = 7171647;
+\Didrive\AUT::$vk_app_secret = 'srJxX0eTaPnGIEnTcCfJ';
+
+// echo '<br/>'.__FILE__.' #'.__LINE__;
+
 if (class_exists('\nyos\msg')) {
+
+// виталя партнер
+    \nyos\Msg::$admins_id[] = 424320836;
+// девушка в КСК Партнер // только на боевой
+    \nyos\Msg::$admins_id[] = 1697422600;
+
 // акк на буке // мой шлак
 //    \nyos\Msg::$admins_id[] = 1368605419;
 // максим
@@ -30,15 +45,18 @@ $db_cfg['host'] = 'localhost';
 //}
 // боевой
 //else {
-
 //    $db_cfg['db'] = 'sql_yadom_admin';
 //    $db_cfg['login'] = 'yadom_admin';
 //    $db_cfg['pass'] = '123Yapdom';
-    $db_cfg['db'] = 'dev_ksk';
-    $db_cfg['login'] = 'ksk';
-    $db_cfg['pass'] = '123Ksk123';
-    
+$db_cfg['db'] = 'dev_ksk';
+$db_cfg['login'] = 'ksk';
+$db_cfg['pass'] = '123Ksk123';
+
 //}
 
 // \f\pa($db_cfg);
 // die(\f\pa($db_cfg));
+
+
+
+// \Nyos\mod\Shop::$mod_items = '010.catalog_items';
